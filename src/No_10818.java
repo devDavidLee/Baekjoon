10c1d8f5ea37ca8d8b4012d629f0865ca1cc035a
Damin Lee <ldm0830@gmail.com>
2021-12-20 22:43:00 +0900

    백준 10818번 최소,최대

---
+++ src/No_10818.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No_10818 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(num);
+        System.out.println(num[0]+" "+num[N-1]);
+    }
+}
