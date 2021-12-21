21a3f3a2334e7649375ee0018f17631919111d2e
Damin Lee <ldm0830@gmail.com>
2021-12-21 11:05:46 +0900

    백준 2577번 숫자의 개수

---
+++ src/No_2577.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class No_2577 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int result = A*B*C;
+        int count=0;
+        String str = Integer.toString(result);
+        String[] array_str = str.split("");
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < str.length(); j++) {
+                if (array_str[j].equals(Integer.toString(i))) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+            count = 0;
+        }
+    }
+}
