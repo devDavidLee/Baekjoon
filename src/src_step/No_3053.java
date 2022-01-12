5ebf7b47341ae4394cd6cf237c3fcfcd8f5b6a7c
Damin Lee <ldm0830@gmail.com>
2022-01-12 20:07:27 +0900

    백준 3053번 택시 기하학

---
+++ src/src_step/No_3053.java
@@ -0,0 +1,12 @@
+package src_step;
+
+import java.util.Scanner;
+
+public class No_3053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double R = sc.nextInt();
+        System.out.println(R*R*Math.PI);
+        System.out.println(2*R*R);
+    }
+}
