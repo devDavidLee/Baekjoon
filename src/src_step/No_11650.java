0a52681301402ef80c42e1d435f246430d84750c
Damin Lee <ldm0830@gmail.com>
2022-02-01 22:33:13 +0900

    백준 11650번 좌표 정렬하기

--- src/src_step/No_11650.java
+++ src/src_step/No_11650.java
@@ -1,6 +1,7 @@
 package src_step;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class No_11650 {
@@ -12,44 +13,18 @@ public class No_11650 {
             arr[i][0] = sc.nextInt();
             arr[i][1] = sc.nextInt();
         }
-        int prev = arr[0][0];
-        int[][] temp = new int[1][2];
-        for (int i = 1; i < N; i++) {
-            if (arr[i][0]<prev){
-                //prev = arr[i][0];
-                temp[0] = arr[i-1];
-                arr[i-1] = arr[i];
-                arr[i] = temp[0];
-            } else {
-                prev = arr[i][0];
-            }
-        }
-        int temp_;
-        prev = arr[0][1];
-        for (int i = 1; i < N; i++) {
-            if (arr[i][0] == arr[i-1][0]){
-                if (arr[i][1]<prev){
-                    temp_ = arr[i-1][1];
-                    arr[i-1][1] = arr[i][1];
-                    arr[i][1] = temp_;
-                } else {
-                    prev = arr[i][1];
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0]==o2[0]){
+                    return o1[1]-o2[1];
+                }else {
+                    return o1[0]-o2[0];
                 }
             }
-            for (int j = 1; j < N; j++) {
-                if (arr[j][1]<arr[j-1][1] && arr[i][0] == arr[i-1][0] && i==N-1){
-                    i=1;
-                    continue;
-                }
-            }
-
-        }
+        });
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
+            System.out.println(arr[i][0]+" "+arr[i][1]);
         }
-        System.out.println(prev);
     }
 }
