04796053e7d47f9e06fc70f6ac5d33c39a89cc3a
Damin Lee <ldm0830@gmail.com>
2022-02-10 19:41:45 +0900

    백준 3046번

---
+++ src/src_step/No_3046.java
@@ -0,0 +1,12 @@
+package src_step;
+
+import java.util.Scanner;
+
+public class No_3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        System.out.println(S*2 - R1);
+    }
+}
