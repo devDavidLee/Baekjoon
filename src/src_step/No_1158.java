d61f4ee5bf746053f3f816c85943399dc8760f87
Damin Lee <ldm0830@gmail.com>
2022-02-15 18:43:16 +0900

    백준 1158번

---
+++ src/src_step/No_1158.java
@@ -0,0 +1,33 @@
+package src_step;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class No_1158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int j = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+        while (queue.size()!=0){
+            if (j%K==0){
+                if (queue.size()==1)
+                    sb.append(queue.poll());
+                else
+                    sb.append(queue.poll()+", ");
+            } else {
+                queue.add(queue.poll());
+            }
+            j++;
+        }
+        sb.append(">");
+        System.out.println(sb);
+    }
+}
