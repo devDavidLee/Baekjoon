4bae01e6f937428abc381b8b8dc1252e5e7453c6
Damin Lee <ldm0830@gmail.com>
2022-02-21 20:37:34 +0900

    백준 5337번

---
+++ src/src_step/No_5337.java
@@ -0,0 +1,9 @@
+package src_step;
+
+public class No_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
