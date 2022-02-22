79e57ea15f9ab3346a83189e371a5da56e7e4e04
Damin Lee <ldm0830@gmail.com>
2022-02-22 18:18:37 +0900

    백준 11942번, 15740번

---
+++ src/src_step/No_11942.java
@@ -0,0 +1,7 @@
+package src_step;
+
+public class No_11942 {
+    public static void main(String[] args) {
+        System.out.println("고려대학교");
+    }
+}
---
+++ src/src_step/No_15740.java
@@ -0,0 +1,13 @@
+package src_step;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class No_15740 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = BigInteger.valueOf(sc.nextInt());
+        BigInteger B = BigInteger.valueOf(sc.nextInt());
+        System.out.println(A.add(B));
+    }
+}
