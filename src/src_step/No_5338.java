7eb55fb3998c34b042894945991c98e487e6bd34
Damin Lee <ldm0830@gmail.com>
2022-02-23 17:29:22 +0900

    백준 5338번

---
+++ src/src_step/No_5338.java
@@ -0,0 +1,11 @@
+package src_step;
+
+public class No_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
