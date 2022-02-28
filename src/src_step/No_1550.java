94358442db57b14db6f4b9c775825168946a2935
Damin Lee <ldm0830@gmail.com>
2022-02-28 20:32:31 +0900

    백준 1550번

---
+++ src/src_step/No_1550.java
@@ -0,0 +1,12 @@
+package src_step;
+
+import java.util.Scanner;
+
+public class No_1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        String str = sc.nextLine();
+        System.out.println(Integer.parseInt(str,16));
+    }
+}
