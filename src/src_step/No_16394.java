2e3de9bfb0f74ba3186134ae378fc0a29201835d
Damin Lee <ldm0830@gmail.com>
2022-03-07 19:14:24 +0900

    백준 16394번

---
+++ src/src_step/No_16394.java
@@ -0,0 +1,11 @@
+package src_step;
+
+import java.util.Scanner;
+
+public class No_16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n-1946);
+    }
+}
