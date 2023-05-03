84668e6800f0ce395971e45d152f331548e171dc
Lee Damin <ldm0830@gmail.com>
2023-05-03 14:48:06 +0000

    No_10845 큐

---
+++ src/python_src/No_10845.py
@@ -0,0 +1,30 @@
+import sys
+
+N = int(input())
+str = [sys.stdin.readline().strip() for i in range(N)]
+queue = list()
+for i in range(0,N):
+    if "push" in str[i]:
+        queue.append(str[i].strip("push").strip())
+    elif "pop" in str[i]:
+        if len(queue)==0:
+            print("-1")
+        else:
+            print(queue.pop(0))
+    elif "size" in str[i]:
+        print(len(queue))
+    elif "empty" in str[i]:
+        if len(queue)==0:
+            print("1")
+        else:
+            print("0")
+    elif "front" in str[i]:
+        if len(queue)==0:
+            print("-1")
+        else:
+            print(queue[0])
+    elif "back" in str[i]:
+        if len(queue)==0:
+            print("-1")
+        else:
+            print(queue[-1])
