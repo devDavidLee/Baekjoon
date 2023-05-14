a2bcad5cdf503bd459d8b5c88dc7063379db1a2a
Lee Damin <ldm0830@gmail.com>
2023-05-14 14:53:36 +0000

    반례 찾아야함

--- src/python_src/No_9012.py
+++ src/python_src/No_9012.py
@@ -5,19 +5,26 @@ VPS_right = ")"
 N = int(input())
 #VPS = [0*i for i in range(0,N)]
 str = [list(map(str, sys.stdin.readline().strip())) for i in range(N)]
+j=0
+
 
 for i in range(0,N):
     str_len = len(str[i])
     if str[i][0]==")":
         str[i][0]=" "
         continue
-    for j in range(0,str_len):
-        if j+1==str_len:
-            continue
-        elif str[i][j]=="(" and str[i][j+1]==")":
+    while j+1<str_len:
+        if str[i][j]=="(" and str[i][j+1]==")":
             str[i][j]="A"
             str[i][j+1]="A"
+            str[i][:] = (s for s in str[i] if s !="A" )
+            str_len = len(str[i])
             j=0
+        else:
+            j=j+1
 
-print(str[0].remove("A"))
-print(str[0])
+for i in range(0,len(str)):
+    if len(str[i])==0:
+        print("YES")
+    else:
+        print("NO")
