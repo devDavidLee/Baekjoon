fdc769fd2d3d8fe40431b76e1e4e3ec6802b70cb
Lee Damin <ldm0830@gmail.com>
2023-05-15 06:34:14 +0000

    No_9012 괄호

--- src/python_src/No_9012.py
+++ src/python_src/No_9012.py
@@ -1,30 +1,48 @@
+# import sys
+
+# N = int(input())
+# input_array = [list(map(str, sys.stdin.readline().strip())) for i in range(N)]
+# j=0
+
+
+# for i in range(0,N):
+#     input_array_len = len(input_array[i])
+#     if input_array[i][0]==")":
+#         input_array[i][0]="A"
+#         continue
+#     while j+1<input_array_len:
+#         if input_array[i][j]=="(" and input_array[i][j+1]==")":
+#             input_array[i][j]="A"
+#             input_array[i][j+1]="A"
+#             input_array[i][:] = (s for s in input_array[i] if s !="A" )
+#             input_array_len = len(input_array[i])
+#             j=0
+#         else:
+#             j=j+1
+
+# for i in range(0,len(input_array)):
+#     if len(input_array[i])==0:
+#         print("YES")
+#     else:
+#         print("NO")
+
 import sys
 
-VPS_left = "("
-VPS_right = ")"
 N = int(input())
-#VPS = [0*i for i in range(0,N)]
-str = [list(map(str, sys.stdin.readline().strip())) for i in range(N)]
-j=0
+input_array = [list(map(str, sys.stdin.readline().strip())) for i in range(N)]
 
-
-for i in range(0,N):
-    str_len = len(str[i])
-    if str[i][0]==")":
-        str[i][0]=" "
-        continue
-    while j+1<str_len:
-        if str[i][j]=="(" and str[i][j+1]==")":
-            str[i][j]="A"
-            str[i][j+1]="A"
-            str[i][:] = (s for s in str[i] if s !="A" )
-            str_len = len(str[i])
-            j=0
-        else:
-            j=j+1
-
-for i in range(0,len(str)):
-    if len(str[i])==0:
+for i in range(N):
+    stack = []
+    for j in range(0,len(input_array[i])):
+        if input_array[i][j]=="(":
+            stack.append(input_array[i][j])
+        elif not stack and input_array[i][j]==")":
+            stack.append(input_array[i][j])
+            break
+        elif stack[-1]=="(" and input_array[i][j]==")":
+            stack.pop()
+        
+    if not stack:
         print("YES")
     else:
         print("NO")
