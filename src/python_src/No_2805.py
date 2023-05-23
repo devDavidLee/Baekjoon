5ebe72ac869abc0840336428cf2d86b4e65d3f6c
Lee Damin <ldm0830@gmail.com>
2023-05-23 15:00:07 +0000

    No_2805번 시간초과...

--- src/python_src/No_2805.py
+++ src/python_src/No_2805.py
@@ -1,8 +1,27 @@
-#머리 안돌아가서 잠시 중단
 import sys
 
 N, M = map(int, input().split())
 tree = list(map(int, sys.stdin.readline().split()))
+tree.sort()
 
-print(N,M)
-print(tree)
+low = 0
+high = len(tree)-1
+
+def sol(N,tree,mid):
+    sum=0
+    for i in range(N):
+        if tree[i]>mid:
+            sum+=tree[i]-mid
+    return sum
+
+while low+1<=high:
+    mid = (tree[low]+tree[high])//2
+    ans = sol(N,tree,mid)
+    if ans==M:
+        break
+    elif ans>M:
+        low = low+1
+    else:
+        high = high-1
+
+print(mid)
