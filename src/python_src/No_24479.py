c9009c18fb3322a72ad32b6458a7ed1b6ea27c90
Lee Damin <ldm0830@gmail.com>
2023-06-14 14:58:02 +0000

    24479번 시간초과

--- src/python_src/No_24479.py
+++ src/python_src/No_24479.py
@@ -1,36 +1,43 @@
-# DFS 메서드 정의
-def dfs (graph, node, visited):
-    visited[node] = True
-    print(node)
-    for i in graph[node]:
-        #print("i=================",i)
-        if not visited[i]:
-            dfs(graph, i, visited)
+import sys
+sys.setrecursionlimit(10 ** 6) 
+
+def dfs(graph, start, visited = []):
+    visited.append(start)
+    for node in graph[start]:
+        if node not in visited:
+            #print("dfs(node:",node,")",end="\n")
+            dfs(graph, node, visited)
+    return visited
 
 N, M, R = map(int, input().split())
-graph_dic = {}
-for i in range(N):
-    a,b = map(int, input().split())
-    graph_dic[a]=b
-    graph_dic[b]=a
 
-print("dic:",graph_dic)
+graph = dict()
+for i in range(1,N+1):
+    graph[i]=[]
+
+for i in range(1,M+1):
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+# graph[1] = [2,4]
+# graph[2] = [1,3,4]
+# graph[3] = [2,4]
+# graph[4] = [1,2,3]
+# graph[5] = []
+
 
 for i in range(1,N+1):
-    if not (i in graph_dic):
-        print(i,": X")
+    graph[i].sort()
+
+result=dfs(graph,1)
+
+for i in range(1,N+1):
+    if i in result:
+        print(result.index(i)+1)
     else:
-        print(i,":",graph_dic[i])
+        print(0)
 
-# graph = [
-#     [],
-#     [2, 4],
-#     [1, 3, 4],
-#     [2, 4],
-#     [1, 2, 3],
-#     [],
-# ]
-
-# visited = [False] * 5
-
-# dfs(graph, 1, visited)
+# print(graph)
+# print(dfs(graph,1))
